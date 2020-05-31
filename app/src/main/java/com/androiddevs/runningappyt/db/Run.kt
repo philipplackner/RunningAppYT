943793c908cf7bb61675fc22edb1a49ae7586bae
Philipp <philipp.lackner@yahoo.de>
2020-05-31 15:37:42 +0200

    Added Run Entity and RunDAO

---
+++ app/src/main/java/com/androiddevs/runningappyt/db/Run.kt
@@ -0,0 +1,18 @@
+package com.androiddevs.runningappyt.db
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "running_table")
+data class Run(
+    var img: Bitmap? = null,
+    var timestamp: Long = 0L,
+    var avgSpeedInKMH: Float = 0f,
+    var distanceInMeters: Int = 0,
+    var timeInMillis: Long = 0L,
+    var caloriesBurned: Int = 0
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
---
+++ app/src/main/java/com/androiddevs/runningappyt/db/RunDAO.kt
@@ -0,0 +1,51 @@
+package com.androiddevs.runningappyt.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTimeInMillis(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed(): LiveData<Float>
+}
+
+
+
+
+
+
+
+
+
+
